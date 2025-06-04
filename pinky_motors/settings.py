af4d11f9c2159618c9368bd2f60e4067de194e4d
Imran Rafi <129622106+SheikhIshere@users.noreply.github.com>
2025-06-04 16:09:19 +0600

    Update settings.py

--- pinky_motors/settings.py
+++ pinky_motors/settings.py
@@ -84,12 +84,16 @@ WSGI_APPLICATION = 'pinky_motors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
+
 
 
 # Password validation
