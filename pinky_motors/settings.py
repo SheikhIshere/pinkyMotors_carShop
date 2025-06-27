2efac2d30bbbec2432bb0a9a6af6e195f1a9dcb2
Imran Rafi <129622106+SheikhIshere@users.noreply.github.com>
2025-06-27 19:16:27 +0600

    Update settings.py csrf link added

--- pinky_motors/settings.py
+++ pinky_motors/settings.py
@@ -18,6 +18,10 @@ load_dotenv()  # This must be before other settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://pinkymotors.onrender.com',
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
